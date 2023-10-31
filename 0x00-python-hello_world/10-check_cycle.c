48ae05b27dfdb0efc7c9055651e6910096cd21cf
Keighme <keighmellow@gmail.com>
2023-10-30 20:48:45 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,9 @@
+#client_loop:
+~
+ssh 962be621b2bd@962be621b2bd.50da2fcc.alx-cod.online
+bbab67516e857c0532ec
+alx-higher_level_programming/
+cd alx-higher_level_programming
+ls
+cd 0x00-python-hello_world/
+
